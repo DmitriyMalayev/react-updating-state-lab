f84d1dff05648b923c0dd0ee859258460523161c
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-02-22 02:33:07 -0500

    commit tree 03f368200eafdd903fe29fca41560aed27f0044d

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,82 @@
-// Code YouTubeDebugger Component Here
+import React from "react";
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p",
+        },
+      },
+    };
+  }
+
+  handleBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12,
+      },
+    });
+  };
+
+  handleResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: "720p",
+        },
+      },
+    });
+  };
+
+  // this.setState({
+  //   addressInfo: Object.assign({}, this.state.addressInfo, {
+  //     city: "New York City",
+  //   }),
+  // });
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleBitrate}></button>,
+        <button className="resolution" onClick={this.handleResolution}></button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
+
+// this.setState({
+//   addressInfo: {
+//     ...this.state.addressInfo,
+//     city: "New York City",
+//   },
+// });
+
+// const house = {
+//   kitchen: {
+//     cabinets: "white",
+//     table: {
+//       legs: 4,
+//     },
+//   },
+// };
+
+// // Note: `deepMerge()` isn't actually a built-in function
+// const updatedHouse = deepMerge(house, {
+//   kitchen: {
+//     table: {
+//       legs: 8,
+//     },
+//   },
+// });
+// // Deeply merging like this would only update the legs property with a value of 8, but the rest of the kitchen and house objects' structure will remain intact.
+
+// // We can solve this using Object.assign() by merging the addressInfo object with the new data ourselves:
